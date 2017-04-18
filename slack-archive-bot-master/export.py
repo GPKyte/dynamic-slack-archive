073e9f22efb91f9cbfb2a9ea2cc5b4f2da0caeb6
Gavin Kyte <gpkyte@gmail.com>
2017-04-18 13:57:18 -0400

    Better export
    Now the export creates the channel folders and updates them with a JSON
    file that contains new messages for the day.

--- slack-archive-bot-master/export.py
+++ slack-archive-bot-master/export.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 import datetime
+import time
 import os
 import sys
 import sqlite3
@@ -9,7 +10,10 @@ import json
 from slackclient import SlackClient
 
 directory = sys.argv[1]
+time = time.time() - 2*86400
 
+def getDate(ts):
+  return datetime.datetime.fromtimestamp(int(ts)).strftime('%Y-%m-%d')
 
 def dict_factory(cursor, row):
   d = {}
@@ -56,28 +60,50 @@ ENV['id_channel'] = dict([(m['id'], m['name']) for m in channels])
 
 
 # Translate database into JSON format
-cursor.execute("SELECT * FROM messages ORDER BY channel, timestamp")
+command = ("SELECT * FROM messages WHERE timestamp > %s ORDER BY channel, timestamp") % time
+cursor.execute(command)
 results = byteify(cursor.fetchall())
 
+channel_msgs = dict([(c['name'], []) for c in channels])
 
 for message in results:
+  message['text'] = message['message']
+  message['ts'] = message['timestamp']
+  message['type'] = 'message'
+  message.pop('message')
+  message.pop('timestamp')
+
   channel_name = get_channel_name(message['channel'])
+  if channel_name == "None":
+    continue
+
+  channel_msgs[channel_name].append(message)
+
+
+# Go to channel folder and title message collection as <date>.json
+for channel_name in channel_msgs.keys():
+  # Checks if any messages today
+  if len(channel_msgs[channel_name]) == 0:
+    continue
+  else:
+    print("%s has been updated") %channel_name
+
   dir = os.path.join(directory, channel_name)
   if "None" in dir:
     print("Channel not found: %s") %message['channel']
     continue
 
-  file = os.path.join(dir, "base_date")
-  if os.path.isfile(file):
-    with open(file, 'r+') as outfile:
-      old_data = json.load(outfile)
-      new_data = old_data.update(message)
-      json.dump(new_data, outfile)
-      outfile.close()
+  if not os.path.exists(dir):
+    os.makedirs(dir)
 
-  else:
-    with open(file, 'r+') as outfile:
-      json.dump(message)
-      outfile.close()
+  timestamp = getDate(channel_msgs[channel_name][0]['ts'].split('.')[0])
+  file = os.path.join(dir, "%s.json") % timestamp
+
+  with open(file, 'w') as outfile:
+    json.dump(channel_msgs[channel_name], outfile)
+    outfile.close()
+
+
+
 
 connection.close()
